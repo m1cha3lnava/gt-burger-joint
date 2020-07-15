46b16884094a8553ab780084fd55d4db8bf22310
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-14 21:24:40 -0400

    handlebars now receives data from the db

--- controllers.js/burgers_controller.js
+++
@@ -1,6 +0,0 @@
-var express = require("express");
-
-var router = express.Router();
-
-// Import the model to use its database functions.
-var burger = require("../models/burger.js");
--- server.js
+++ server.js
@@ -5,19 +5,26 @@ const { selectAll } = require("./config/orm");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory to be served
-app.use(express.static("app/public"));
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-// Routes
-// =============================================================
-// require("./app/routes/api-routes.js")(app);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
